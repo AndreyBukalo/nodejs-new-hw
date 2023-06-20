d780850bb096be010283c589f04490363882a8b9
Andrey Bukalo <andreybukalo@MacBook-Air-Andrey.local>
2023-06-20 14:50:21 +0300

    new template

--- app.js
+++ app.js
@@ -1,42 +1,25 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-require("dotenv").config();
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const contactsRouter = require('./routes/api/contacts')
 
-const app = express();
+const app = express()
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/api/contacts', contactsRouter)
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message })
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
+module.exports = app
